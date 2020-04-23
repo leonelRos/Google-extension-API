b39951758cecdd0f74fa21fcd6379cb1d23b623a
Leonel Rosales <rleonel426@gmail.com>
2020-04-22 19:50:16 -0700

    adding images

--- js/main.js
+++ js/main.js
@@ -40,4 +40,8 @@ function setClock() {
 function setRotation(element, rotationRatio) {
     element.style.setProperty('--rotation', rotationRatio * 360)
 }
+
+function setBackImage() {
+
+}
 setClock();
